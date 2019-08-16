78310af4a285a12d86a51891ca66103d08320552
sgal <zhangsigao@live.com>
2019-08-16 14:58:34 +0800

    备份部署的secret

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = '12#j2lmrlax@8n=!cq)+8e@o(ug4+xeke91xz0yruuxsxodi(7'
+# heroku production secret: h7&0-k0se195z8w^-e8r8(qx%u(_xvz^!-w@7gj(=ijqi4ckjy
 import os
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
